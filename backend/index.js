9f2483e4691a2679d5dbdd15995a06c02fbb64d8
unknown <memogarcia246@gmail.com>
2023-09-13 14:58:11 -0600

    add routes to app router and connect to db

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,11 @@ import { jsonParser, urlEncodedParser } from './middleware/bodyParser.js'
 import connectDB from './config/db.js'
 import cors from 'cors'
 import dotenv from 'dotenv'
+import authRouter from './routes/authRoutes.js'
+import userRouter from './routes/userRoutes.js'
+import groupRouter from './routes/groupRoutes.js'
+import expenseRouter from './routes/expenseRoutes.js'
+import transactionRouter from './routes/transactionRoutes.js'
 
 dotenv.config()
 const PORT = process.env.PORT || 3001
@@ -14,10 +19,14 @@ app.use(jsonParser)
 app.use(urlEncodedParser)
 
 // Database connection
-// connectDB()
+connectDB()
 
 // Routes
-// ...
+app.use('/api/auth', authRouter)
+app.use('/api/users', userRouter)
+app.use('/api/groups', groupRouter)
+app.use('/api/expenses', expenseRouter)
+app.use('/api/transactions', transactionRouter)
 
 // Start server
 app.listen(PORT, () => {
