c927d68d44608e56270e7daf3287faffdfe91bdb
unknown <memogarcia246@gmail.com>
2023-09-15 15:22:30 -0600

    add Group route to App

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router-dom'
 import Login from './pages/Login'
 import Register from './pages/Register'
 import Home from './pages/Home'
+import Group from './pages/Group'
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Route path='/' element={<Home />} />
       <Route path='login' element={<Login />} />
       <Route path='register' element={<Register />} />
+      <Route path='group/:groupid' element={<Group />} />
     </Routes>
   );
 }
