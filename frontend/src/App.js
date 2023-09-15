3727d4e70a04fde2670f797657e8409ff88bda29
Jonas Clausen <jonastclausen@gmail.com>
2023-09-15 16:20:09 -0600

    new expense setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,13 +2,14 @@ import { Routes, Route } from 'react-router-dom'
 import Login from './pages/Login'
 import Register from './pages/Register'
 import Home from './pages/Home'
-
+import NewExpense from './pages/NewExpense'
 function App() {
   return (
     <Routes>
       <Route path='/' element={<Home />} />
       <Route path='login' element={<Login />} />
       <Route path='register' element={<Register />} />
+      <Route path='newexpense' element={<NewExpense />} />
     </Routes>
   );
 }
