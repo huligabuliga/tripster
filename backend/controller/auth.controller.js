d555a7010c9635fecb4059808eacb2dcd6ca8a20
Guillermo Andrés García Vázquez <43385064+MemazoG@users.noreply.github.com>
2023-09-15 17:00:06 -0600

    Revert "Group page"

--- backend/controller/auth.controller.js
+++ backend/controller/auth.controller.js
@@ -26,32 +26,29 @@ export const register = async (req, res, next) => {
 
 export const login = async (req, res) => {
     try {
-      const user = await User.findOne({ email: req.body.email });
-      if (!user) {
-        return res.status(400).json({ message: 'user not found' });
-      }
-  
-      const isCorrect = bcrypt.compareSync(req.body.password, user.password);
-      if (!isCorrect) {
-        return res.status(400).json({ message: 'password incorrect' });
-      }
-  
-      // json web token
-      const token = jwt.sign({
-        id: user._id,
-      }, process.env.JWT_SECRET);
-  
-      const { password, ...info } = user._doc;
-  
-      res.cookie('accessToken', token, {
-        httpOnly: true,
-      });
-  
-      res.status(200).json(info);
+        const user = await User.findOne({username: req.body.username})
+        if(!user){
+            res.status(400).json({message: "user not found"})    
+        }
+
+        const isCorrect = bcrypt.compareSync(req.body.password, user.password)
+            if(!isCorrect){
+                res.status(400).json({message: "password incorrect"})
+            }
+
+            // json web token
+            const token = jwt.sign({
+                id: user._id,
+            }, process.env.JWT_SECRET);
+        const {password, ...info} = user._doc
+        res.cookie("accessToken", token, {
+            httpOnly: true,
+        }).status(200).json(user)
+
     } catch (err) {
-      res.status(500).json({ message: err.message });
+        res.status(500).json({message: err.message})
     }
-  };
+}
 
 export const logout = async (req, res) => {
     res.clearCookie("accessToken", {
--- backend/index.js
+++ backend/index.js
@@ -15,7 +15,7 @@ const PORT = process.env.PORT || 3001
 const app = express()
 
 // Middlewares
-app.use(cors({origin: 'http://localhost:3000', credentials: true}))
+app.use(cors())
 app.use(jsonParser)
 app.use(urlEncodedParser)
 
