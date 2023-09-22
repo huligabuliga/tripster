41364fe00579937bd9d9515cf0327d1cde028659
unknown <memogarcia246@gmail.com>
2023-09-22 14:32:03 -0600

    constants file

---
+++ frontend/src/utils/constants.js
@@ -0,0 +1,33 @@
+export const categoryList = [
+    { value: 'Food', label: 'Food'},
+    { value: 'Drinks', label: 'Drinks'},
+    { value: 'Transportation', label: 'Transportation'},
+    { value: 'Housing', label: 'Housing'},
+    { value: 'Entertainment', label: 'Entertainment'},
+    { value: 'Shopping', label: 'Shopping'},
+    { value: 'Travel', label: 'Travel'},
+    { value: 'Gift', label: 'Gift'},
+    { value: 'Miscellaneous/Other', label: 'Miscellaneous/Other'},
+  ]
+
+export const currencyList = [
+    { value: 'MXN', label: 'MXN - Mexican Peso'},
+    { value: 'USD', label: 'USD - US Dollar'},
+    { value: 'EUR', label: 'EUR - European Euro'},
+    { value: 'CAD', label: 'CAD - Canadian Dollar'},
+    { value: 'AUD', label: 'AUD - Australian Dollar'},
+    { value: 'GBP', label: 'GBP - Pound Sterling'},
+    { value: 'SEK', label: 'SEK - Swedish Krona'},
+    { value: 'JPY', label: 'JPY - Japanese Yen'},
+    { value: 'CHF', label: 'CHF - Swiss Franc'},
+    { value: 'CNY', label: 'CNY - Chinese Yuan'},
+]
+
+export const groupMembers = [
+    { _id: '1235645fd', username: 'Juan' },
+    { _id: '1rgdvsdf4', username: 'Pedro' },
+    { _id: '1012iedsf', username: 'Miguel Ángel' },
+    { _id: '9nv9w8fwe', username: 'Guillermo' },
+    { _id: '678uhjsdf', username: 'Daniel' },
+    { _id: '6tfhjui9i', username: 'Mario' },
+]
