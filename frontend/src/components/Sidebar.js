8286d1e030f6c355ce0419e06ec95a9ec1b61fc3
Jonas Clausen <jonastclausen@gmail.com>
2023-11-17 01:46:36 -0600

    currency on pay page

--- frontend/src/components/Sidebar.js
+++ frontend/src/components/Sidebar.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import '../navbar.css'
+// import '../navbar.css'
 import { Link, useLocation } from 'react-router-dom'
 
 const Sidebar = ({ userId }) => {
--- frontend/src/pages/Pay.js
+++ frontend/src/pages/Pay.js
@@ -1,16 +1,56 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
+import { categoryList, currencyList } from '../utils/constants';
+import Select from 'react-select'
 import axios from 'axios';
 
 const PaymentPage = () => {
   const [imageUrl, setImageUrl] = useState('');
   const navigate = useNavigate();
+  const [expense, setExpense] = useState(null);
   const [isPaid, setIsPaid] = useState(false);
+  const [targetCurrency, setTargetCurrency] = useState(''); // Add this line
+  const [conversionRate, setConversionRate] = useState(1); 
   const { groupId, expenseId } = useParams(); // Get the expenseId from the URL
   const currentUser = JSON.parse(localStorage.getItem('currentUser'));
   console.log('currentUser:', currentUser);
   console.log('currentUser._id:', currentUser._id);
   console.log('expenseId:', expenseId)
+  const [currency, setCurrency] = useState('');
+
+
+//   conversion 
+useEffect(() => { // Add this useEffect
+    const fetchConversionRate = async () => {
+      if (expense && targetCurrency) {
+        try {
+          const response = await axios.get(`https://api.exchangerate-api.com/v4/latest/${expense.currency}`);
+          setConversionRate(response.data.rates[targetCurrency]);
+        } catch (err) {
+          console.error(err);
+        }
+      }
+    };
+
+    fetchConversionRate();
+  }, [expense, targetCurrency]);
+
+  useEffect(() => { 
+    const fetchExpense = async () => {
+      try {
+        const response = await axios.get(`http://localhost:3001/api/expenses/${expenseId}`);
+        setExpense(response.data);
+        console.log('Expense:', response.data);
+      } catch (err) {
+        console.error(err);
+      }
+    };
+
+    fetchExpense();
+  }, [expenseId]);
+
+
+//   pay button
   const handlePay = async (event) => {
     event.preventDefault();
     try {
@@ -31,9 +71,39 @@ const PaymentPage = () => {
     }
   };
 
+
+  // Update state of selected currency
+  const handleCurrencyChange = (selectedCurrency) => {
+    setTargetCurrency(selectedCurrency.value); // Update this line
+    setCurrency(selectedCurrency);
+  }
+
+
   return (
     <div className='w-full min-h-screen flex flex-col items-center justify-center'>
       <form onSubmit={handlePay} className='w-3/4 sm:w-1/2 lg:w-1/3 xl:w-1/4'>
+      {expense && ( // Add this block
+          <div>
+            <p>Amount to pay: {expense.totalAmount}</p>
+            <p>Currency: {expense.currency}</p>
+          </div>
+        )}
+
+        <div>
+        <Select
+              options={currencyList}
+              value={currency}
+              onChange={handleCurrencyChange}
+              closeMenuOnSelect={true}
+              className='bg-gray-100 outline-none cursor-pointer w-2/3'
+            />
+        </div>
+        <div>
+            <p>Converted Amount to pay: </p>
+            {/* converted cost */}
+            <p>Converted Amount to pay: {expense && (expense.totalAmount * conversionRate).toFixed(2)} </p> 
+        </div>
+
         <label className='block'>
           <span className='text-gray-700'>Evidence:</span>
           <input type="text" value={imageUrl} onChange={e => setImageUrl(e.target.value)} 
