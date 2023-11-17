ff77365098cace7995cf1ec95a37a1f5029a9850
David Macias <43385146+dmacias77@users.noreply.github.com>
2023-11-17 01:08:31 -0600

    New sidebar design

--- frontend/src/components/Sidebar.js
+++ frontend/src/components/Sidebar.js
@@ -1,58 +1,62 @@
 import React from 'react'
+import '../navbar.css'
 import { Link, useLocation } from 'react-router-dom'
-import { FaHome, FaSearch, FaBell, FaUser } from 'react-icons/fa'
 
 const Sidebar = ({ userId }) => {
   const location = useLocation();
-    
+
   return (
-    (location.pathname === '/login' || location.pathname === '/register') ? (<div></div>) : (
-    <div className='h-screen w-80 flex flex-col bg-green-500'>
-        {/** Logo */}
-        <Link to='/'>
-            <img src='/cf_logo_wh.png' className='w-full h-auto p-2' alt="Coinfusion Logo"></img>
-        </Link>
-
-        {/** Home */}
-        <div>
-            <Link to={`/home/${userId}`}>
-                <div className='flex flex-row justify-center py-5 text-slate-50'>
-                    <FaHome className=" text-3xl mx-4" />
-                    <h2 className='font-semibold text-xl flex-grow'>Home</h2>
-                </div>
-            </Link>
-        </div>
-
-        {/** Search */}
-        <div>
-            <Link to='/search'>
-                <div className='flex flex-row justify-center pb-5 text-slate-50'>
-                    <FaSearch className=" text-3xl mx-4" />
-                    <h2 className='font-semibold text-xl flex-grow'>Search</h2>
-                </div>
-            </Link>
-        </div>
-
-        {/** Notifications */}
-        <div>
-            <Link to='/notifications'>
-                <div className='flex flex-row justify-center pb-5 text-slate-50'>
-                    <FaBell className=" text-3xl mx-4" />
-                    <h2 className='font-semibold text-xl flex-grow'>Notifications</h2>
-                </div>
-            </Link>
-        </div>
-
-        {/** My Account */}
-        <div>
-            <Link to={`/profile/${userId}`}>
-                <div className='flex flex-row justify-center text-slate-50'>
-                    <FaUser className=" text-3xl mx-4" />
-                    <h2 className='font-semibold text-xl flex-grow'>My Account</h2>
-                </div>
-            </Link>
-        </div>
-    </div>
+    (location.pathname === '/login' | location.pathname === '/register') ? (<div></div>) : (
+      <div>
+        <input id="hamburger" className="hamburger" type="checkbox" />
+        <label htmlFor="hamburger" className="hamburger">
+          <i></i>
+          <text>
+            <close>close</close>
+            <open>menu</open>
+          </text>
+        </label>
+        <nav className="leftnav">
+          <ul>
+            {/* Home */}
+            <li>
+              <Link to={`/home/${userId}`}>
+                <img className="icon" src="/cf_home_icon.png" alt="Home" />
+                Home
+              </Link>
+            </li>
+            {/* Search */}
+            <li>
+              <Link to='/search'>
+                <img className="icon" src="/search_icon.png" alt="Search" />
+                Search
+              </Link>
+            </li>
+            {/* Notifications */}
+            <li>
+              <Link to='/notifications'>
+                <img className="icon" src="/bell_icon.png" alt="Notifications" />
+                Notifications
+                {/* <div className="tag">1</div> */}
+              </Link>
+            </li>
+            {/* My Account */}
+            <li>
+              <Link to={`/profile/${userId}`}>
+                <img className="icon" src="/profile_icon.png" alt="My Account" />
+                My Account
+              </Link>
+            </li>
+            {/* Logout */}
+            <li>
+              <Link to='/'>
+                <img className="icon" src="/logout_icon.png" alt="Logout" />
+                Logout
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
     )
   )
 }
