d582657ba02f10c801ef1c100c4cea787384aa6d
Jonas Clausen <jonastclausen@gmail.com>
2023-11-17 05:01:51 -0600

    wiwiuwuiwuwuwuwu works

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,23 @@
-import { useState, useEffect } from 'react';
-import { Routes, Route } from 'react-router-dom';
-import { useMediaQuery } from './hooks/hooks';
-import Login from './pages/Login';
-import Register from './pages/Register';
-import Home from './pages/Home';
-import Group from './pages/Group';
-import NewExpense from './pages/NewExpense';
-import SettleUp from './pages/SettleUp';
-import JoinGroup from './pages/JoinGroup';
-import Navbar from './components/Navbar';
-import Sidebar from './components/Sidebar';
-import Profile from './pages/Profile';
-import EditProfile from './pages/EditProfile';
-import NewGroup from './pages/NewGroup';
-import UserInfo from './pages/GetUser';
-import NotFound from './pages/NotFound'
-import RequireAuth from './components/RequireAuth';
-import Members from './pages/Members';
-import Pay from './pages/Pay';
-
+import { useState, useEffect } from "react";
+import { Routes, Route } from "react-router-dom";
+import { useMediaQuery } from "./hooks/hooks";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Home from "./pages/Home";
+import Group from "./pages/Group";
+import NewExpense from "./pages/NewExpense";
+import SettleUp from "./pages/SettleUp";
+import JoinGroup from "./pages/JoinGroup";
+import Navbar from "./components/Navbar";
+import Sidebar from "./components/Sidebar";
+import Profile from "./pages/Profile";
+import EditProfile from "./pages/EditProfile";
+import NewGroup from "./pages/NewGroup";
+import UserInfo from "./pages/GetUser";
+import NotFound from "./pages/NotFound";
+import RequireAuth from "./components/RequireAuth";
+import Members from "./pages/Members";
+import Pay from "./pages/Pay";
 
 function App() {
   const [userId, setUserId] = useState(null);
@@ -26,49 +25,51 @@ function App() {
 
   useEffect(() => {
     // Check if user data is already stored in localStorage
-    const currentUser = JSON.parse(localStorage.getItem('currentUser'));
+    const currentUser = JSON.parse(localStorage.getItem("currentUser"));
     if (currentUser) {
       setUserId(currentUser._id);
     } else {
       // Fetch user data and set the userId state variable
-      fetch('/api/user')
-        .then(response => response.json())
-        .then(data => {
+      fetch("/api/user")
+        .then((response) => response.json())
+        .then((data) => {
           if (data.userId) {
             setUserId(data.userId);
+            // Store the user data in localStorage
+            localStorage.setItem("currentUser", JSON.stringify(data));
           }
         })
-        .catch(error => console.error(error));
+        .catch((error) => console.error(error));
     }
   }, []);
 
   return (
-    <div className='flex flex-col sm:flex-row'>
+    <div className="flex flex-col sm:flex-row">
       {/** If bigger than mobile, display sidebar */}
       {!isMobile && <Sidebar userId={userId} />}
       <Routes>
         {/** Public routes */}
-        <Route path='login' element={<Login />} />
-        <Route path='register' element={<Register />} />
+        <Route path="login" element={<Login />} />
+        <Route path="register" element={<Register />} />
 
         {/** Protected routes */}
-        <Route element={<RequireAuth />} >
-          <Route path='' element={<Home />} />
-          <Route path='group/:groupId' element={<Group />} />
-          <Route path='group/:groupId/newExpense' element={<NewExpense />} />
-          <Route path='group/:groupId/members' element={<Members />} />
-          <Route path='settleup' element={<SettleUp />} />
-          <Route path='joinGroup' element={<JoinGroup />} />
-          <Route path='profile/:userid' element={<Profile />} />
-          <Route path='profile/:userid/edit' element={<EditProfile />} />
-          <Route path='newGroup' element={<NewGroup />} />
-          <Route path='users/:userId' exact component={UserInfo} />
+        <Route element={<RequireAuth />}>
+          <Route path="" element={<Home />} />
+          <Route path="group/:groupId" element={<Group />} />
+          <Route path="group/:groupId/newExpense" element={<NewExpense />} />
+          <Route path="group/:groupId/members" element={<Members />} />
+          <Route path="group/:groupId/settleup" element={<SettleUp />} />
+          <Route path="joinGroup" element={<JoinGroup />} />
+          <Route path="profile/:userid" element={<Profile />} />
+          <Route path="profile/:userid/edit" element={<EditProfile />} />
+          <Route path="newGroup" element={<NewGroup />} />
+          <Route path="users/:userId" exact component={UserInfo} />
           {/* pay page */}
-          <Route path='/group/:groupId/pay/:expenseId' element={<Pay />} />
+          <Route path="/group/:groupId/pay/:expenseId" element={<Pay />} />
         </Route>
-        
-        { /** Catch all route */}
-        <Route path='*' element={<NotFound />} />
+
+        {/** Catch all route */}
+        <Route path="*" element={<NotFound />} />
       </Routes>
       {/** If mobile size, display bottom navbar */}
       {isMobile && <Navbar />}
--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,91 +1,93 @@
-import React, { useState } from 'react';
-import { Link, useNavigate, useLocation } from 'react-router-dom'
-import { FaRegEnvelope, FaKey } from "react-icons/fa"
+import React, { useState } from "react";
+import { Link, useNavigate, useLocation } from "react-router-dom";
+import { FaRegEnvelope, FaKey } from "react-icons/fa";
 import axios from "axios";
-import newRequest from '../utils/newRequest'
-import useAuth from '../hooks/useAuth';
-import '../cftools.css';
+import newRequest from "../utils/newRequest";
+import useAuth from "../hooks/useAuth";
+import "../cftools.css";
 
 const Login = () => {
-    const { setAuth } = useAuth()
-    const [email, setEmail] = useState('')
-    const [password, setPassword] = useState('')
-    const [error, setError] = useState(null)
+  const { setAuth } = useAuth();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(null);
 
-    const navigate = useNavigate()
-    const location = useLocation()
-    const from = location.state?.from?.pathname || '/'
+  const navigate = useNavigate();
+  const location = useLocation();
+  const from = location.state?.from?.pathname || "/";
 
-    const handleSubmit = async (e) => {
-        e.preventDefault()
-        try {
-            const res = await axios.post('http://localhost:3001/api/auth/login', JSON.stringify({email, password}),
-                {
-                    headers: { 'Content-Type': 'application/json' },
-                    withCredentials: true
-                }
-            )
-
-            // Store
-            setAuth({ id: res.data._id, user: email, password})
-
-            // Navigate to homepage or previous route (any other page user tried to access but was unauthenticated)
-            navigate(from, { replace: true })
-        } 
-        catch (error) {
-            setError(error.response.data.message)
-            console.log(error)
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await axios.post(
+        "http://localhost:3001/api/auth/login",
+        JSON.stringify({ email, password }),
+        {
+          headers: { "Content-Type": "application/json" },
+          withCredentials: true,
         }
+      );
+
+      // Store
+      setAuth({ id: res.data._id, user: email, password });
+      localStorage.setItem("currentUser", JSON.stringify(res.data));
+
+      // Navigate to homepage or previous route (any other page user tried to access but was unauthenticated)
+      navigate(from, { replace: true });
+    } catch (error) {
+      setError(error.response.data.message);
+      console.log(error);
     }
+  };
   return (
-    <div className='justify-center cfbg_01 h-screen w-full flex items-center'>
-            <div className='flex flex-col justify-center  max-w-[400px] w-full px-2 py-3 mx-auto text-center bg-slate-50 rounded-lg shadow-lg shadow-gray-400'>
-                <img src='/coinfusionLogoGreen.jpg' alt='Coinfusion Logo'></img>
-                <h2 className='text-3xl font-bold mb-2'>Log In</h2>
-            <form onSubmit={handleSubmit}>
-                <div className="flex justify-center mb-2">
-                    <div className="bg-gray-100 flex items-center w-64 p-2">
-                        <FaRegEnvelope className="mr-2" />
-                        <input
-                        type="email"
-                        name="email"
-                        placeholder="Email"
-                        className="bg-gray-100 outline-none flex-1 cursor-pointer"
-                        onChange={e=>setEmail(e.target.value)}
-                    />
-                    </div>
-                </div>
-
-                <div className="flex justify-center">
-                    <div className="bg-gray-100 flex items-center w-64 p-2">
-                        <FaKey className="mr-2" />
-                    <input
-                        type="password"
-                        name="password"
-                        placeholder="Password"
-                        className="bg-gray-100 outline-none flex-1"
-                        onChange={e=>setPassword(e.target.value)}
-                    />
-                    </div>
-                </div>
-
-                <button
-                    className="bg-green-600 rounded-full font-semibold text-white px-12 py-2 my-2 cursor-pointer !important"
-                    type="submit"
-                >
-                    Log In
-                </button>
-                {error && <p className="text-red-500">{error}</p>}
-</form>
-                <p>
-                    Don't have an account?{" "}
-                    <Link to="/register" className="font-semibold text-green-600">
-                        Register!
-                    </Link>
-                </p>
+    <div className="justify-center cfbg_01 h-screen w-full flex items-center">
+      <div className="flex flex-col justify-center  max-w-[400px] w-full px-2 py-3 mx-auto text-center bg-slate-50 rounded-lg shadow-lg shadow-gray-400">
+        <img src="/coinfusionLogoGreen.jpg" alt="Coinfusion Logo"></img>
+        <h2 className="text-3xl font-bold mb-2">Log In</h2>
+        <form onSubmit={handleSubmit}>
+          <div className="flex justify-center mb-2">
+            <div className="bg-gray-100 flex items-center w-64 p-2">
+              <FaRegEnvelope className="mr-2" />
+              <input
+                type="email"
+                name="email"
+                placeholder="Email"
+                className="bg-gray-100 outline-none flex-1 cursor-pointer"
+                onChange={(e) => setEmail(e.target.value)}
+              />
             </div>
-        </div>
-  )
-}
+          </div>
 
-export default Login
+          <div className="flex justify-center">
+            <div className="bg-gray-100 flex items-center w-64 p-2">
+              <FaKey className="mr-2" />
+              <input
+                type="password"
+                name="password"
+                placeholder="Password"
+                className="bg-gray-100 outline-none flex-1"
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </div>
+          </div>
+
+          <button
+            className="bg-green-600 rounded-full font-semibold text-white px-12 py-2 my-2 cursor-pointer !important"
+            type="submit"
+          >
+            Log In
+          </button>
+          {error && <p className="text-red-500">{error}</p>}
+        </form>
+        <p>
+          Don't have an account?{" "}
+          <Link to="/register" className="font-semibold text-green-600">
+            Register!
+          </Link>
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
